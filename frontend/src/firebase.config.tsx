905e093d9e8a2a99e8ecf4a45b0c9d2a022034c0
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-11-30 01:02:00 -0500

    enable Google Analytics just because I'm curious how much attention my submission gets hehe

--- frontend/src/firebase.config.tsx
+++ frontend/src/firebase.config.tsx
@@ -1,4 +1,5 @@
 import { initializeApp } from '@firebase/app';
+import { getAnalytics, logEvent } from '@firebase/analytics';
 import { getFunctions, connectFunctionsEmulator } from '@firebase/functions';
 import secrets from './secrets.json';
 
@@ -14,6 +15,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const functions = getFunctions(app);
+const analytics = getAnalytics(app);
+logEvent(analytics, 'page_view');
 
 if (process.env.NODE_ENV === 'development') {
   connectFunctionsEmulator(functions, 'localhost', 5001);
